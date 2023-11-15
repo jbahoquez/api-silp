512711fa4c4f28998d31396a16c739aa65ff1f8d
jbahoquez <jbahoquez@gmail.com>
2023-11-15 12:47:42 -0500

    Funcional

--- src/seguridad/intranet-usuarios/services/intranet-usuarios.service.ts
+++ src/seguridad/intranet-usuarios/services/intranet-usuarios.service.ts
@@ -6,12 +6,14 @@ import { IntranetUsuario } from '../entities/intranet-usuario.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UpdatePasswordIntranetUsuarioDto } from '../dto/update-password-intranet-usuario.dto';
-import oracledb from 'oracledb';
+import * as  oracledb from 'oracledb';
+import { RolesModule } from '../../roles/roles.module';
 
 @Injectable()
 export class IntranetUsuariosService {
   constructor(
     @InjectRepository(IntranetUsuario) private readonly intranetUsuarioRepository: Repository<IntranetUsuario>,
+    
   ) { }
 
 
@@ -60,31 +62,115 @@ export class IntranetUsuariosService {
   }
 
   async getUserBy1(data: UpdatePasswordIntranetUsuarioDto): Promise<IIntranetUsuarios[]> {
-    const v_data = JSON.stringify(data);
 
-    const query = `
-    DECLARE
-      v_data CLOB;
-      v_salida CLOB;
-    BEGIN
-      v_data := :v_data;
-      SP_INTRANET_USUARIOS_CONSULTAR(v_data, v_salida);
-      :v_salida := v_salida;
-    END;
-  `;
+    
+
+    let user: IIntranetUsuarios[] = null
+    //let v_salida
+    try{
+      const v_data = JSON.stringify(data)//`{n_ide:''1143453248'',password:''2223DDD''}`;
+      console.log(JSON.stringify(data))
+  //   let v_salida;
+  //   const query = `
+  //   DECLARE
+  //     v_data CLOB;
+  //     v_salida CLOB;
+  //   BEGIN
+  //     v_data := :v_data;
+  //     PKG_SEGURIDAD.SP_INTRANET_USUARIOS_CONSULTAR(v_data, v_salida);
+  //     DBMS_OUTPUT.PUT_LINE('V_SALIDA = ' || V_SALIDA);
+  //     :v_salida := v_salida;
+  //   END;
+  // `;
 
     // const user: IIntranetUsuarios = await this.intranetUsuarioRepository.query(
     //   `BEGIN SP_INTRANET_USUARIOS_CONSULTAR(${v_data}, :v_salida); END;`
     // );
     // return user;
-    const result = await this.intranetUsuarioRepository.query(query, [v_data, { type: 'CLOB', dir: oracledb.BIND_OUT }]);
+    // const input={v_data, v_salida}
+    // const result = await this.intranetUsuarioRepository.query(query, 
+    //   [input]);
+    // const oracledb=require("oracledb")
+    // const conn= await oracledb.getConnection();
 
-  const user: IIntranetUsuarios[] = result[0].v_salida; // Obtén el resultado del procedimiento almacenado
+    // const result2 = await conn.execute(
+    //   `BEGIN PKG_SEGURIDAD.SP_INTRANET_USUARIOS_CONSULTAR(:v_data, :v_salida); END;`,
+    //   [
+    //     { name: 'v_data',val: v_data, type: oracledb.CLOB, dir: oracledb.BIND_IN },
+    //     { name: 'v_salida',type: oracledb.CLOB, dir: oracledb.BIND_OUT }
+    //   ]
+    // )
+
+
+    const connection = await oracledb.getConnection({
+      user: 'plasticosta',
+      password: 'calidad',
+      connectString: `
+      (DESCRIPTION = 
+        (FAILOVER=ON) 
+       (ADDRESS=(PROTOCOL=tcp)(HOST=192.168.162.43)(PORT=1521)) 
+            (CONNECT_DATA =
+         (SERVICE_NAME =PLAS)
+       )
+     )
+        `,
+    });
+
+    // const v_data = JSON.stringify(data); // Tu entrada de datos
+    const query = `BEGIN PKG_SEGURIDAD.SP_INTRANET_USUARIOS_CONSULTAR(:v_data, :v_salida); END;`;
+    const bindVars = {
+      v_data: v_data,
+      v_salida: { type: oracledb.CLOB, dir: oracledb.BIND_OUT },
+    };
+    const result2 = await connection.execute(query, bindVars);
+    // await connection.close();
+    // const result = await connection.execute(`SELECT * FROM INTRANET_USUARIOS WHERE N_IDE='1143453248'`);
+    // await connection.close();
+    console.log(result2)
+    
+    
+//     const v_salida: any  = result.outBinds;
+// const clobData = await v_salida.getData();
+// const clobString = clobData.toString('utf8');
+// console.log(clobString)
+
+    const result: any = await this.intranetUsuarioRepository.query(
+      `BEGIN PKG_SEGURIDAD.SP_INTRANET_USUARIOS_CONSULTAR(:v_data, :v_salida); END;`,
+      [
+        { name: 'v_data',val: v_data, type: oracledb.CLOB, dir: oracledb.BIND_IN },
+        { name: 'v_salida',type: oracledb.DB_TYPE_CLOB, dir: oracledb.BIND_OUT }
+      ]
+    )//.then((res)=>console.log(res[0].getRow()));
+
+    // const result: any = await this.intranetUsuarioRepository.query(
+    //   `SELECT * FROM INTRANET_USUARIOS WHERE N_IDE='1143453248'`
+    // )
+    //  console.log(result[0])
+
+     
+
+      
+
+
+
+
+    // const result = await this.intranetUsuarioRepository.query(query,[data,{type: oracledb.STRING, dir: oracledb.BIND_OUT, maxsize:1073741822}]);
+
+
+  // user = result.v_salida; // Obtén el resultado del procedimiento almacenado
+  // console.log(result.outBinds.v_salida)
+    }catch (error){
+      console.error('Error al ejecutar el procedimiento almacenado:', error);
+      return user
+    }
+    
 
   return user;
   }
 
 
 }
+
+
 
 
